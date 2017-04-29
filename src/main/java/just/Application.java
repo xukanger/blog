7fd0f14605f90552a2ba5c35b596992f16196a7d
xukanger <552177156@qq.com>
2017-04-29 13:14:49 +0800

    初步框架搭建

---
+++ src/main/java/just/Application.java
@@ -0,0 +1,24 @@
+package just;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by yt on 2017/4/25.
+ */
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
