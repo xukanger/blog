6997a4292e03ad4954b09fb32f47f024c5da0236
xukanger <552177156@qq.com>
2017-05-09 10:04:22 +0800

    图形验证码

--- src/main/java/just/web/AuthController.java
+++ src/main/java/just/web/AuthController.java
@@ -1,5 +1,6 @@
 package just.web;
 
+import com.google.code.kaptcha.Producer;
 import just.VO.JSONResult;
 import just.VO.jwt.JwtAuthenticationRequest;
 import just.VO.jwt.JwtAuthenticationResponse;
@@ -8,12 +9,21 @@ import just.service.auth.AuthService;
 import just.util.CookieUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class AuthController {
@@ -25,6 +35,13 @@ public class AuthController {
 
     @Autowired
     private AuthService authService;
+
+    @Autowired
+    private Producer captchaProducer;
+
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
+
 
     //获取token登录
     @RequestMapping(value = "/auth", method = RequestMethod.POST)
@@ -72,6 +89,49 @@ public class AuthController {
             return JSONResult.fillResultString(null,"有敏感词",false);
         else
             return JSONResult.fillResultString(null,"成功",true);
+    }
+
+
+    @RequestMapping(value = "/captcha-image")
+    public ModelAndView getKaptchaImage(HttpServletRequest request,
+                                        HttpServletResponse response) throws IOException {
+        response.setDateHeader("Expires", 0);
+        response.setHeader("Cache-Control",
+                "no-store, no-cache, must-revalidate");
+        response.addHeader("Cache-Control", "post-check=0, pre-check=0");
+        response.setHeader("Pragma", "no-cache");
+        response.setContentType("image/jpeg");
+
+        String capText = captchaProducer.createText();
+        try {
+            String uuid= UUID.randomUUID().toString();
+            redisTemplate.opsForValue().set(uuid, capText,60*5, TimeUnit.SECONDS);
+            Cookie cookie = new Cookie("captchaCode",uuid);
+            response.addCookie(cookie);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        BufferedImage bi = captchaProducer.createImage(capText);
+        ServletOutputStream out = response.getOutputStream();
+        ImageIO.write(bi, "jpg", out);
+        try {
+            out.flush();
+        } finally {
+            out.close();
+        }
+        return null;
+    }
+
+    @RequestMapping(value = "/verifyCode" , method = RequestMethod.POST)
+    public String verifyCode(@RequestParam String code,
+                             @CookieValue("captchaCode") String captchaCode){
+        String correctCode = redisTemplate.opsForValue().get(captchaCode);
+        if(correctCode==null)
+            return JSONResult.fillResultString(null,"验证码失效",false);
+        else if(correctCode.equals(code))
+            return JSONResult.fillResultString(null,"成功",true);
+        else
+            return JSONResult.fillResultString(null,"失败",false);
     }
 
     private void addAuthCookie(HttpServletResponse response,String token){
