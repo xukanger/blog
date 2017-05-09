039b551b6fe9bf40973477d8280eb418bd5da025
xukanger <552177156@qq.com>
2017-05-09 15:13:22 +0800

    bug修改

--- src/main/java/just/VO/JSONResult.java
+++ src/main/java/just/VO/JSONResult.java
@@ -11,6 +11,8 @@ public class JSONResult {
 
     private Object result;
 
+
+
     public JSONResult(Integer status, String message, Object result) {
         this.status = status;
         this.message = message;
@@ -20,5 +22,29 @@ public class JSONResult {
     public static JSONResult fillResultString(Integer status, String message, Object result){
 
         return new JSONResult(status,message,result);
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getResult() {
+        return result;
+    }
+
+    public void setResult(Object result) {
+        this.result = result;
     }
 }
