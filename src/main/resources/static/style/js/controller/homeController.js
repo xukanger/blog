9fedd3078927d6232c611766e379a540e21d244b
teen1996 <1172593703@qq.com>
2017-05-16 15:37:52 +0800

    controller

--- src/main/resources/static/style/js/controller/homeController.js
+++ src/main/resources/static/style/js/controller/homeController.js
@@ -1,6 +1,23 @@
 /**
  * Created by tanghao on 2017/5/13.
  */
-myApp.controller('homeController', ['$scope', '$filter', '$routeParams', '$route',function ($scope, $filter, $routeParams,$route) {
+myApp.controller('homeController', ['$scope', '$filter', '$routeParams', '$route','userService','articleService',function ($scope, $filter, $routeParams,$route,userService,articleService) {
+
+    $scope.user={
+        "username":'',
+        "password":'',
+    };
+    $scope.articles=[
+        {
+
+        }
+    ];
+
+    $scope.doLogin=function () {
+        console.log("login begin");
+        userService.login($scope.user).then(function (data) {
+            alert(data);
+        });
+    };
 
 }]);
--- src/main/resources/static/style/js/controller/serivce.js
+++ src/main/resources/static/style/js/controller/serivce.js
@@ -2,3 +2,29 @@
  * Created by tanghao on 2017/5/13.
  */
 /*接口管理JS*/
+var myAppServie = angular.module('myApp.service', []);
+
+myAppServie.factory('userService', ['$http', '$q', function($http, $q) {
+    return {
+        login:function(user){
+            var deferred = $q.defer();
+            $http.post('/auth', user).success(
+                function(response){
+                    if(response){
+                        deferred.resolve(response);
+                    }else{
+                        deferred.reject(response);
+                    }
+                }
+            ).error(function (reason) {
+                deferred.reject(reason);
+            });
+            return deferred.promise;
+        }
+    }
+}]);
+
+myAppServie.factory('articleService', ['$http', '$q', function($http, $q) {
+    return {};
+
+}]);
