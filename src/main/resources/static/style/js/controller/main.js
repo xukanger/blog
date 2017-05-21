18cbc84905fdbea37f8d41a67c860b619b56e604
LengJunT <lengroubao@163.com>
2017-05-21 15:23:49 +0800

    style 重新提交

--- src/main/resources/static/style/js/controller/main.js
+++ src/main/resources/static/style/js/controller/main.js
@@ -26,34 +26,30 @@ myApp.config(['$routeProvider',function($routeProvider){
             })
             .otherwise({redirectTo:'/'});
     }]);
-myApp.controller('appCintroller', ['$scope', '$filter', '$routeParams','$route', 'userService','articleService',function ($scope, $filter, $routeParams,$route,userService,articleService) {
+myApp.controller('appController', ['$scope', '$filter', '$routeParams','$route', 'userService','articleService',function ($scope, $filter, $routeParams,$route,userService,articleService) {
     $scope.isLogin = false;
     //TODO:抽取方法，页面间跳转使用
-    var token = getCookie("token");
+    var token = getCookie("Authorization");
+    console.dir(token);
     if(token!=""){
-        userService.login(token).then(function (data) {
-                console.dir(data);
-                if(data){
-                    token = data;
-                    $scope.isLogin = true;//其他页面需要根据登录状况判断是否要转回登录页面
-                    setCookie("token",token);//FIXME
-                    console.log("login success");
-
-                }
-            },
-            function (reason) {
-                console.dir(reason);
-                if(reason.status == "403"){
-                    console.log("token invalid");
-                    return;
-                }
+        userService.getSelf().then(function (data) {
+            console.dir(data);
+            $scope.self = data.result;
+            $scope.isLogin = true;
+        },function (data) {
+            if(data.status == "403"){
+                console.log("access denied");
+                return;
             }
-        );
+        })
     }
 
     $scope.user={
         "username":'',
         "password":'',
+    };
+    $scope.self={
+
     };
     $scope.articles=[
         {
@@ -81,13 +77,6 @@ myApp.controller('appCintroller', ['$scope', '$filter', '$routeParams','$route',
         );
     };
 
-    function setCookie(name,value)
-    {
-        var Days = 7;
-        var exp = new Date();
-        exp.setTime(exp.getTime() + Days*24*60*60*1000);
-        document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString();
-    }
 
     function getCookie(name)
     {
--- src/main/resources/static/style/js/controller/userhomeController.js
+++ src/main/resources/static/style/js/controller/userhomeController.js
@@ -2,5 +2,10 @@
  * Created by tanghao on 2017/5/13.
  */
 myApp.controller('userhomeController', ['$scope', '$filter', '$routeParams','$route', function ($scope, $filter, $routeParams,$route) {
-
+    var newArticleListArry = [
+        {id:1,title:'sdfa',heat:10000},
+        {id:2,title:'lol出装',heat:200},
+        {id:3,title:'lol天赋',heat:3200}
+    ]
+    $scope.newArticleListArry = newArticleListArry;
 }]);
